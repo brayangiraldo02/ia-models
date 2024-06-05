ecb1dda919d92b8167c80558ce1b98765832d647
SMejia115 <s.mejia6@utp.edu.co>
2024-06-05 05:37:55 -0500

    Version 2 implementation

--- back/version1.py
+++ back/version1.py
@@ -11,7 +11,8 @@ model1_router = APIRouter()
 class_names = [chr(i) for i in range(48, 58)] + [chr(i) for i in range(65, 91)] + [chr(i) for i in range(97, 123)]
 
 # Cargar el modelo guardado
-model = load_model('emnist_saved_model3.h5')
+model1 = load_model('mnist.h5')
+model2 = load_model('emnist_final_model.keras')
 
 # Definir el esquema del cuerpo de la solicitud
 class ImageData(BaseModel):
@@ -30,7 +31,34 @@ async def predict(data: ImageData):
         arr = arr.reshape(1, 28, 28, 1)
 
         # Realizar y obtener la predicción
-        prediction_values = model.predict(arr, batch_size=1)
+        prediction_values = model1.predict(arr, batch_size=1)
+        for i in range(len(prediction_values[0])):
+            print(class_names[i] + ": " + str(prediction_values[0][i]))
+        prediction_index = np.argmax(prediction_values)
+        prediction = class_names[prediction_index]
+        print("Prediccion final: " + prediction)
+
+        return {"prediction": prediction}
+
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+
+@model1_router.post("/predict2/")
+async def predict(data: ImageData):
+    try:
+        # Limpiar los datos recibidos
+        data = parse.unquote(data.pixeles)
+
+        # Realizar transformación para dejar igual que los ejemplos que usa EMNIST
+        arr = np.fromstring(data, np.float32, sep=",")
+        arr = arr.reshape(28, 28)
+        arr = np.array(arr)
+        arr = arr.reshape(1, 28, 28, 1)
+
+        # Realizar y obtener la predicción
+        prediction_values = model2.predict(arr, batch_size=1)
         for i in range(len(prediction_values[0])):
             print(class_names[i] + ": " + str(prediction_values[0][i]))
         prediction_index = np.argmax(prediction_values)
